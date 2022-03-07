8009717dd9e6922b8d57af28d1d9c487dc4f0ece
heejinkim908 <heejin.kim.dev@gmail.com>
2022-03-08 00:50:30 +0900

    feat: Add delete todo function

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import ToDo from "../components/ToDo";
 import { actionCreators } from "../store";
 
 function Home({ toDos, addToDo }) {
@@ -27,7 +28,11 @@ function Home({ toDos, addToDo }) {
         />
         <button>Add</button>
       </form>
-      <ul>{JSON.stringify(toDos)}</ul>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo {...toDo} key={toDo.id} />
+        ))}
+      </ul>
     </>
   );
 }
--- src/store.js
+++ src/store.js
@@ -3,33 +3,37 @@ import { createStore } from "redux";
 const ADD = "ADD";
 const DELETE = "DELETE";
 
-//action: addToDo
+// action: addToDo
 const addToDo = (text) => {
   return {
     type: ADD,
     text,
   };
 };
-//action: deleteToDo
+// action: deleteToDo
 const deleteToDo = (id) => {
   return {
     type: DELETE,
-    id,
+    id: parseInt(id),
   };
 };
 
+// reducer
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD:
       return [...state, { text: action.text, id: Date.now() }];
     case DELETE:
-      return state.filter((toDo) => toDo !== action.id);
+      return state.filter((toDo) => toDo.id !== action.id);
     default:
       return state;
   }
 };
 
+// store
 const store = createStore(reducer);
+
+// export
 
 export const actionCreators = {
   addToDo,
