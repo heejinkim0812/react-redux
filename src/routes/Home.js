f0c10dc156ebf8968758b7ac07bb92876a88deb8
heejinkim908 <heejin.kim.dev@gmail.com>
2022-03-07 23:49:15 +0900

    feat: Add mapStateProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 
-function Home() {
+function Home({ toDos }) {
   const [text, setText] = useState("");
+
   function onChange(e) {
     setText(e.target.value);
   }
+
   function onSubmit(e) {
     e.preventDefault();
     console.log(text);
     setText("");
   }
+
   return (
     <>
       <h1>To Do</h1>
@@ -22,7 +26,14 @@ function Home() {
         />
         <button>Add</button>
       </form>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
-export default Home;
+
+// mapStateToProp : store의 state 받아오는 함수 => Home component에 prop으로 전달
+function mapStateToProp(state) {
+  return { toDos: state };
+}
+
+export default connect(mapStateToProp)(Home);
